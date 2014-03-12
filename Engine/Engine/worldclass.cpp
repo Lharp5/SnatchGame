d72930246ed0de1c5f859052f93e6fb5d6fa271c
Luke Harper <lukeharper@live.ca>
2014-03-12 12:40:44 -0400

    Flooring
    Working on Changing the floor and roof rendering to the world class
    since every level will always have the same floor.

--- Engine/Engine/worldclass.cpp
+++ Engine/Engine/worldclass.cpp
@@ -5,3 +5,33 @@
 *	Date:		feb. 21/14
 **************************/
 
+#include "worldclass.h"
+
+WorldClass::WorldClass()
+{
+	renderModels =0;
+
+	initalizeWorld();
+}
+
+void WorldClass::initalizeWorld()
+{
+	initalizeRoof();
+	initalizeFloor();
+}
+
+void WorldClass::initalizeRoof()
+{
+}
+
+void WorldClass::initalizeFloor()
+{
+	for(int i=0; i<100; i++){
+		for(int j=0; j<100; j++){
+			floor[i][j] =  new QuadTexturedModel(10.0f, 10.0f, L"../Engine/textures/die1.dds");
+			floor[i][j]->worldTranslate(10.0f*i, -5.0f,10.0f*j);
+			floor[i][j]->worldRotateX(XM_PIDIV2);
+		}
+	}
+	
+}
--- Engine/Engine/worldclass.h
+++ Engine/Engine/worldclass.h
@@ -5,17 +5,27 @@
 
 #include "levelclass.h"
 
-class worldClass
+class WorldClass
 {
 public:
+	WorldClass();
+	~WorldClass();
 	void runGame();
 	void getModels(ArrayList<GameModel>*); //models to be sent back to the system for rendering.
 private:
 	//objects
 	LevelClass levels;
 	ArrayList<GameModel>* renderModels; //Lists of models that can be stored so they can be rendered.
+	GameModel* floor[100][100];
+	GameModel* roof[100][100];
 
 	//functions
+
+	//setting up the default world.
+	void initalizeWorld();
+	void initalizeRoof();
+	void initalizeFloor();
+
 	void Menu();
 	void playGame();
 	void advanceLevel();
