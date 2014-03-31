c7b20860e871d551e1e46bb042ca7a17432ed6df
Luke Harper <lukeharper@live.ca>
2014-03-31 16:36:46 -0400

    Created UI Class
    Added a score enum type.

---
+++ Engine/Engine/UI.h
@@ -0,0 +1,15 @@
+/* UI.H*/
+#ifndef UI_H
+#define UI_H
+
+#include "quadTexturedModel.h"
+#include "worldDefs.h"
+
+class UI
+{
+public:
+private:
+	
+};
+
+#endif
--- Engine/Engine/worldDefs.h
+++ Engine/Engine/worldDefs.h
@@ -8,17 +8,19 @@ enum GameState { MENU, PLAYING, WIN, GAMEOVER };
 
 enum Direction { NORTH, EAST, SOUTH, WEST };
 
+enum score {FAIL = 0, OK, GOOD,GREAT, EXCELL};
+
 //Explanation for the numbers attached to WALL_TYPEs:
 ////representation of which direction the object is facing
 ////1 means facing positive z
 ////2 means facing positive x
 ////3 means facing negative z
 ////4 means facing negative x
-typedef enum {	C_DOOR_1, C_DOOR_2,
+enum WALL_TYPE {C_DOOR_1, C_DOOR_2,
 				C_LOCK_1, C_LOCK_2, C_LOCK_3, C_LOCK_4,
 				C_LIGHT_1, C_LIGHT_2, C_LIGHT_3, C_LIGHT_4,
 				C_WALL,
 				C_FLOOR,
-				C_DEFAULT} WALL_TYPE;
+				C_DEFAULT};
 
 #endif
